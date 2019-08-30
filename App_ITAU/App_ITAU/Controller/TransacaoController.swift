2cc47ec553dbbe5150b28e9e68340961ba7531a4
Alexandre Aun <alexandreaun@gmail.com>
2019-08-30 01:26:00 -0300

    finalizado configuração de seção e linhas com nomes dos contatos

--- App_ITAU/App_ITAU/Controller/TransacaoController.swift
+++ App_ITAU/App_ITAU/Controller/TransacaoController.swift
@@ -14,13 +14,65 @@ class TransacaoController{
     var saldoCC: Double = 0
     var saldoPoupanca: Double = 0
     
+    // MARK: - Cria quantidade de linhas da tableView Contacts
+    func numberOfrowsContact()->Int{
+        
+        return jasonserializer.serealizationJson().count
+    }
     
+    
+    // MARK: - Retorna a primeira letra de cada nome, dentro de um Array
+    func firstLetterContact() -> [String]{
+        
+        var firstLetterArray: [String] = []
+        
+          for firstLetter in jasonserializer.serealizationJson(){
+            
+          //  firstLetterArray = jasonserializer.serealizationJson().filter({$0.name.prefix(1)})
+            
+            firstLetterArray.append(String(firstLetter.name.prefix(1)))
+            
+        }
+        print(firstLetterArray)
+        return firstLetterArray
+        
+    }
+    
+    // MARK: - Retorna apenas uma letra inicial de cada nome, ou seja, numberOfSections
+    func removeDuplicates() -> [String]{
+        
+        let uniqueItems = NSOrderedSet(array: firstLetterContact())
+        print(uniqueItems)
+        
+        return uniqueItems.array as? [String] ?? []
+    }
+    
+    
+    // MARK: - Filtra os nomes dos contatos em cada seção
+    func filterNamesforSection(section: Int) -> [Contact]{
+        
+        var namesForSection: [Contact] = []
+        
+        for _section in 0...removeDuplicates().count{
+            
+            if _section == section{
+                
+                namesForSection = jasonserializer.serealizationJson().filter({$0.name.prefix(1) == removeDuplicates()[_section]})
  
+            }
+        }
+        return namesForSection
+    }
     
-    
-    
-    
-    
+    // MARK: - Coleta o nome do contato
+    func contactName(contact: Contact) -> String{
+        
+        var label: String = ""
+        
+        label = contact.name
+        
+        return label
+    }
     
     
 }
--- App_ITAU/App_ITAU/View/TransacaoViewController.swift
+++ App_ITAU/App_ITAU/View/TransacaoViewController.swift
@@ -11,25 +11,93 @@ import UIKit
 class TransacaoViewController: UIViewController {
 
     @IBOutlet weak var tableviewContas: UITableView!
-    
     @IBOutlet weak var tableviewContact: UITableView!
-    
-
+    let transacaocontroller = TransacaoController()
+    let jasonserializer = JsonSerializer()
     
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-      
+     // self.tableviewContas.delegate = self
+     // self.tableviewContas.dataSource = self
+      self.tableviewContact.delegate = self
+      self.tableviewContact.dataSource = self
         
     }
     
+  
     
-    
-    
-    
-    
-
-
 }
+
+
+
+extension TransacaoViewController: UITableViewDelegate, UITableViewDataSource{
+
+    
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return transacaocontroller.removeDuplicates().count
+    }
+    
+    func tableView(_ tableView: UITableView, titleForHeaderInSection section: Int) -> String? {
+        
+        var titleSection: String = ""
+        
+        for _section in 0...transacaocontroller.removeDuplicates().count{
+            
+            if section == _section{
+                
+                titleSection = transacaocontroller.removeDuplicates()[_section]
+                
+            }
+            
+        }
+        
+        return titleSection
+    }
+    
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+       
+        var numberSection: Int = 0
+        
+         for _section in 0...transacaocontroller.removeDuplicates().count{
+        
+            if section == _section{
+                
+                 numberSection = jasonserializer.serealizationJson().filter({$0.name.prefix(1) == transacaocontroller.removeDuplicates()[_section]}).count
+                
+            }
+  
+        }
+ 
+             return numberSection
+
+    }
+        
+        
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ContactTableViewCell", for: indexPath)
+        
+        let filterNamesSection = transacaocontroller.filterNamesforSection(section: indexPath.section)
+        
+        cell.textLabel?.text = transacaocontroller.contactName(contact: filterNamesSection[indexPath.row])
+        
+        return cell
+            
+        }
+    
+    
+    
+    
+}
+    
+    
+    
+    
+    
+
 
